0a44fe27b2df445b0c50ec155f84fed78729d557
me@gototicmail.com <root>
2024-06-03 11:55:09 +0200

    Add fetch

--- asteroids/asteroids.js
+++ asteroids/asteroids.js
@@ -3,7 +3,27 @@ import zim from "https://zimjs.org/cdn/016/zim"
 let frame = new Frame(FIT, 1000, 1000, dark, light, start,
 	["background.jpg", "starship.png", "thrust.png", "asteroid1.png", "asteroid2.png", "shipboom.png", "shoot.png", "boom.png"]);
 
-function start() {
+async function start() {
+
+	const url = 'https://jfkdwsauephidoqkieqf.supabase.co/rest/v1/scores';
+	const apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impma2R3c2F1ZXBoaWRvcWtpZXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4MDYxNTIsImV4cCI6MjAzMjM4MjE1Mn0.E714fIwl6KjHLr9DQFk3aYqkD9GyA3RcMvUffVMaXtA';
+	let response = await fetch(url + '?apikey=' + apikey);
+	let data = await response.json();
+	console.log(data);
+	let maxScore = data.map(item => item.score).reduce((accu, item) => Math.max(accu, item));
+	console.log(maxScore);
+
+	// Types of requests: GET / POST
+	// Asynchronous functions
+
+	function incrementScore(inc) {
+		score = score + inc;
+		scoreLabel.text = score;
+		if (score > maxScore) {
+			scoreLabel.color = red;
+		}
+	}
+
 	function createAsteroid() {
 		if (Math.random() > 0.2) {
 			let asteroid = new Sprite(
@@ -74,6 +94,7 @@ function start() {
 	}
 
 	function spaceCraftHitBy(asteroid) {
+		// return false;
 		return spacecraft.hitTestCircle(asteroid);
 	}
 
@@ -118,14 +139,12 @@ function start() {
 	function fillProjectileStockpile() {
 		if (projectilesStockpile.length < projectileCapacity) {
 			addProjectile();
-			setTimeout(fillProjectileStockpile, 2000);
-			console.log(projectilesStockpile.length);
+			console.log(`Stock: ${projectilesStockpile.length}`);
 		}
 	}
 
 	function shoot() {
 		if (projectilesStockpile.length > 0) {
-			projectileFillProgressBar.percent = 100 * projectilesStockpile.length / projectileCapacity;
 			frame.update();
 			let projectile = projectilesStockpile.at(0);
 			projectile.centerReg();
@@ -135,11 +154,10 @@ function start() {
 			projectile.speed = 10;
 			shootedProjectiles.push(projectile);
 			projectilesStockpile.splice(0, 1);
-			console.log(projectilesStockpile.length);
-			if (projectilesStockpile.length == 0) {
-				setTimeout(fillProjectileStockpile, 3000);
-			}
+			console.log(`Stock: ${projectilesStockpile.length}`);
+			console.log(`Shooted: ${shootedProjectiles.length}`);
 		}
+		projectileFillProgressBar.percent = 100 * projectilesStockpile.length / projectileCapacity;
 	}
 
 	function moveProjectile(projectile) {
@@ -152,6 +170,7 @@ function start() {
 			moveProjectile(shootedProjectiles[i]);
 			if (shootedProjectiles[i].y < 0 - shootedProjectiles[i].height - 50) {
 				shootedProjectiles[i].dispose();
+				shootedProjectiles[i] = null;
 				shootedProjectiles.splice(i, 1);
 			}
 			i = i + 1;
@@ -175,7 +194,8 @@ function start() {
 					call: () => { explosion.dispose(); }
 				});
 				asteroids.splice(i, 1);
-				score = score + 1;
+				incrementScore(5);
+				score = score + 5;
 				scoreLabel.text = score;
 				return true;
 			}
@@ -189,8 +209,9 @@ function start() {
 		while (i < shootedProjectiles.length) {
 			if (projectileHitsAsteroid(shootedProjectiles[i])) {
 				shootedProjectiles[i].dispose();
+				shootedProjectiles[i] = null;
 				shootedProjectiles.splice(i, 1);
-				break;
+				// break;
 			}
 			i = i + 1;
 		}
@@ -239,7 +260,8 @@ function start() {
 
 	let asteroids = [];
 	let projectilesStockpile = [];
-	let projectileCapacity = 5;
+	let projectileCapacity = 20;
+	let refillProjectileDelay = 1000;
 	let projectileFillProgressBar = new ProgressBar({ barType: "rectangle", autoHide: false });
 	projectileFillProgressBar.show();
 	projectileFillProgressBar.loc(frame.stage.width - projectileFillProgressBar.width - 10, 20);
@@ -257,7 +279,20 @@ function start() {
 	endGameLabel.centerReg();
 	endGameLabel.removeFrom(frame.stage);
 
+	let newRecordLabel = new Label({
+		text: `CONGRATULATIONS!\nYou set a new record score!`,
+		size: 50,
+		bold: true,
+		color: red,
+		align: 'center'
+	});
+	newRecordLabel.centerReg();
+	newRecordLabel.mov(0, -200);
+	newRecordLabel.removeFrom(frame.stage);
+
 	let createAsteroidInterval = null;
+	let updateScoreInterval = null;
+	let refillProjectileStockpileInterval = null;
 	function playGame() {
 		createAsteroidInterval = setInterval(createAsteroid, 200);
 		frame.stage.addEventListener("stagemousemove", moveSpaceCraft);
@@ -267,16 +302,26 @@ function start() {
 		Ticker.add(spaceCraftHit);
 		Ticker.add(checkAllProjectiles);
 		Ticker.add(moveProjectiles);
+		updateScoreInterval = setInterval(() => { incrementScore(1); }, 2000)
+		refillProjectileStockpileInterval = setInterval(fillProjectileStockpile, refillProjectileDelay);
 	}
 
-	function restart() {
+	async function restart() {
+		response = await fetch(url + '?apikey=' + apikey);
+		data = await response.json();
+		console.log(data);
+		maxScore = data.map(item => item.score).reduce((accu, item) => Math.max(accu, item));
+		console.log(maxScore);
+
 		spacecraft.addTo(frame.stage);
 		frame.stage.addEventListener("stagemousemove", moveSpaceCraft);
 		frame.stage.addEventListener("click", shoot);
 		endGameLabel.removeFrom(frame.stage);
+		newRecordLabel.removeFrom(frame.stage);
 		frame.stage.removeEventListener("keydown", restart);
 		score = 0;
 		scoreLabel.text = score;
+		scoreLabel.color = green;
 		lives = 3;
 		fillLives();
 		projectilesStockpile = [];
@@ -284,14 +329,37 @@ function start() {
 		shootedProjectiles = [];
 		projectileFillProgressBar.percent = 100 * projectilesStockpile.length / projectileCapacity;
 		createAsteroidInterval = setInterval(createAsteroid, 200);
+		updateScoreInterval = setInterval(() => { incrementScore(1); }, 2000);
+		refillProjectileStockpileInterval = setInterval(fillProjectileStockpile, refillProjectileDelay);
+		document.removeEventListener("keydown", restart);
 	}
 
-	function endGame() {
+	async function endGame() {
 		frame.stage.removeEventListener("stagemousemove", moveSpaceCraft);
 		frame.stage.removeEventListener("click", shoot);
 		endGameLabel.addTo(frame.stage);
+		if (score > maxScore) {
+			newRecordLabel.addTo(frame.stage);
+		}
 		document.addEventListener("keydown", restart);
 		clearInterval(createAsteroidInterval);
+		createAsteroidInterval = null;
+		clearInterval(updateScoreInterval);
+		updateScoreInterval = null;
+		clearInterval(refillProjectileStockpileInterval);
+		refillProjectileStockpileInterval = null;
+		let currentDate = new Date();
+		let currentTimestamptz = currentDate.toISOString();
+		let response = await fetch(url + "?apikey=" + apikey, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({ created_at: currentTimestamptz, score: score })
+		});
+		let data = await response.text();
+		console.log(data);
 	}
 
 	playGame();
